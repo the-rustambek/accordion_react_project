936859b0c491d4fb80ac74e9baff21cdd467ce3e
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2022-01-05 13:45:41 +0500

    add

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+// import questions from "./data";
+import data from "./data";
+import SingleQuestion from "./Question";
+
+function App(){
+  const [questions,setQuestions] = useState(data);
+
+  return(
+    <main>
+        <div className="container">
+            <h3>
+              Question and Answers About Login
+            </h3>
+            <section className="info">
+                {questions.map((question) =>{
+                  return <SingleQuestion key={question.id} {...question}>
+
+                  </SingleQuestion>
+                })}
+            </section>
+        </div>
+    </main>
+  )
+}
+export default App;
